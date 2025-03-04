dc47c2caa4a64284eb4126b56611e2ff5c46adaf
Trinity Quirk <tquirk@ciq.com>
2025-03-03 17:28:46 -0800

    Simplify ISO builder config handling
    We pass boatloads of arguments into the ISO builder object, and keep an
    even larger number in the object itself, almost all of which are copied
    verbatim out of the existing config blobs.  Instead, let's do the config
    handling outside of the builder, put it into an AttributeDict, and
    reference items directly from the collected configuration.  The only
    downside is that this diff is immense; we change several of the scripts,
    and a huge number of lines within the IsoBuild class.
    
    The configs are all collected together in a new method called
    `collect_config` in the common module.  It takes the base config (which
    up to now has been constant and unmodifiable), overlays the
    configuration for the desired release, and further overlays the
    command-line arguments, all into the same structure.  The resulting
    object is a copy of the individual configs, so can be modified without
    affecting the main configurations.  This also opens up the ability to
    override some of the global configuration inside each release config.
    
    We had one name collision in combining the groups of config items:  the
    base config grabs the architecture of the machine it's running on and
    calls it `arch`, and most of the scripts we're modifying also take in an
    `--arch` command-line argument, which also lands in `arch`.  It seems to
    matter which one we want in the places we use them, so we change the
    command-line argument to land in an attribute called `build_arch`.
    
    There were a couple instances we noticed of instance attributes never
    being used, which allowed us to delete them.  There were another couple
    instances of instance attributes being used only once; we inlined these
    definitions and removed the attributes.
    
    This sort of change may be useful for operations which don't use the
    IsoBuild, but that would make for an even larger diff.  We can modify
    the other classes individually in the future.

--- iso/empanadas/empanadas/scripts/build_iso.py
+++ iso/empanadas/empanadas/scripts/build_iso.py
@@ -2,32 +2,23 @@
 
 import argparse
 
-from empanadas.common import config, rldict
+import empanadas.common
 from empanadas.util import IsoBuild
 
-parser = argparse.ArgumentParser(description="ISO Compose")
 
-parser.add_argument('--release', type=str, help="Major Release Version or major-type (eg 9-beta)", required=True)
-parser.add_argument('--isolation', type=str, help="mock isolation mode")
-parser.add_argument('--rc', action='store_true', help="Release Candidate, Beta, RLN")
-parser.add_argument('--local-compose', action='store_true', help="Compose Directory is Here")
-parser.add_argument('--logger', type=str)
-parser.add_argument('--hashed', action='store_true')
-results = parser.parse_args()
-rlvars = rldict[results.release]
-major = rlvars['major']
+def parse_args():
+    parser = argparse.ArgumentParser(description="ISO Compose")
 
-a = IsoBuild(
-        rlvars,
-        config,
-        major=major,
-        rc=results.rc,
-        isolation=results.isolation,
-        compose_dir_is_here=results.local_compose,
-        hashed=results.hashed,
-        logger=results.logger,
-)
+    parser.add_argument('--release', type=str, help="Major Release Version or major-type (eg 9-beta)", required=True)
+    parser.add_argument('--isolation', type=str, choices=['auto', 'simple'], dest='mock_isolation', help="mock isolation mode")
+    parser.add_argument('--rc', action='store_true', help="Release Candidate, Beta, RLN")
+    parser.add_argument('--local-compose', action='store_true', help="Compose Directory is Here")
+    parser.add_argument('--logger', type=str)
+    parser.add_argument('--hashed', action='store_true')
+    return parser.parse_args()
 
 
 def run():
-    a.run()
+    args = parse_args()
+    cfg = empanadas.common.collect_config(args)
+    IsoBuild(cfg).run()
--- iso/empanadas/empanadas/scripts/pull_unpack_tree.py
+++ iso/empanadas/empanadas/scripts/pull_unpack_tree.py
@@ -2,41 +2,28 @@
 
 import argparse
 
-from empanadas.common import *
+import empanadas.common
 from empanadas.util import IsoBuild
 
-parser = argparse.ArgumentParser(description="ISO Artifact Builder")
 
-parser.add_argument('--release', type=str, help="Major Release Version", required=True)
-parser.add_argument('--s3', action='store_true', help="S3")
-parser.add_argument('--rc', action='store_true', help="Release Candidate")
-parser.add_argument('--arch', type=str, help="Architecture")
-parser.add_argument('--local-compose', action='store_true', help="Compose Directory is Here")
-parser.add_argument('--force-unpack', action='store_true', help="Force an unpack")
-parser.add_argument('--force-download', action='store_true', help="Force a download")
-parser.add_argument('--s3-region', type=str, help="S3 region (overrides defaults)")
-parser.add_argument('--s3-bucket', type=str, help="S3 bucket name (overrides defaults)")
-parser.add_argument('--s3-bucket-url', type=str, help="S3 bucket url (overrides defaults)")
-parser.add_argument('--logger', type=str)
-results = parser.parse_args()
-rlvars = rldict[results.release]
-major = rlvars['major']
+def parse_args():
+    parser = argparse.ArgumentParser(description="ISO Artifact Builder")
 
-a = IsoBuild(
-        rlvars,
-        config,
-        major=major,
-        rc=results.rc,
-        s3=results.s3,
-        arch=results.arch,
-        force_unpack=results.force_unpack,
-        force_download=results.force_download,
-        compose_dir_is_here=results.local_compose,
-        s3_region=results.s3_region,
-        s3_bucket=results.s3_bucket,
-        s3_bucket_url=results.s3_bucket_url,
-        logger=results.logger,
-)
+    parser.add_argument('--release', type=str, help="Major Release Version", required=True)
+    parser.add_argument('--s3', action='store_true', help="S3")
+    parser.add_argument('--rc', action='store_true', help="Release Candidate")
+    parser.add_argument('--arch', type=str, dest='build_arch', help="Architecture")
+    parser.add_argument('--local-compose', action='store_true', help="Compose Directory is Here")
+    parser.add_argument('--force-unpack', action='store_true', help="Force an unpack")
+    parser.add_argument('--force-download', action='store_true', help="Force a download")
+    parser.add_argument('--s3-region', type=str, help="S3 region (overrides defaults)")
+    parser.add_argument('--s3-bucket', type=str, help="S3 bucket name (overrides defaults)")
+    parser.add_argument('--s3-bucket-url', type=str, help="S3 bucket url (overrides defaults)")
+    parser.add_argument('--logger', type=str)
+    return parser.parse_args()
+
 
 def run():
-    a.run_pull_lorax_artifacts()
+    args = parse_args()
+    cfg = empanadas.common.collect_config(args)
+    IsoBuild(cfg).run_pull_lorax_artifacts()
